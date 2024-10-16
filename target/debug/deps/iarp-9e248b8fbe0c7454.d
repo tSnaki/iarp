379e037f837bccd242b64f98554d1c876cef256a
Snaki-boi <non469521@gmail.com>
2024-10-16 18:22:12 -0500

    This is the first commit with an empty rust project

---
+++ target/debug/deps/iarp-9e248b8fbe0c7454.d
@@ -0,0 +1,5 @@
+/home/snaki/project/rust/iarp/target/debug/deps/libiarp-9e248b8fbe0c7454.rmeta: src/main.rs
+
+/home/snaki/project/rust/iarp/target/debug/deps/iarp-9e248b8fbe0c7454.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/iarp-ad48dfd96279c56c.d
@@ -0,0 +1,5 @@
+/home/snaki/project/rust/iarp/target/debug/deps/libiarp-ad48dfd96279c56c.rmeta: src/main.rs
+
+/home/snaki/project/rust/iarp/target/debug/deps/iarp-ad48dfd96279c56c.d: src/main.rs
+
+src/main.rs:
